6c322c91d267d0d382e2db915b2d41e7b2b03b82
yuxin.gong <yuxin.gong@capitalonline.net>
2023-03-21 11:29:05 +0800

    add snat alarm

--- pkg/utils/common.go
+++ pkg/utils/common.go
@@ -1,17 +1,18 @@
 package utils
 
 import (
+	"cds-k8s-tools/pkg/oscmd"
+	"fmt"
 	"os"
 )
 
 const (
-	defaultApiHost         = "http://cdsapi.capitalonline.net"
-	defaultApiHostOversea  = "http://cdsapi-us.capitalonline.net"
-	preApiHost             = "http://cdsapi-gateway.gic.pre/openapi"
-	apiHostLiteral         = "CDS_API_HOST"
+	defaultApiHost = "http://cdsapi.capitalonline.net"
+	// defaultApiHostOversea  = "http://cdsapi-us.capitalonline.net"
+	preApiHost = "http://cdsapi-gateway.gic.pre/openapi"
+	// apiHostLiteral         = "CDS_API_HOST"
 	accessKeyIdLiteral     = "CDS_ACCESS_KEY_ID"
 	accessKeySecretLiteral = "CDS_ACCESS_KEY_SECRET"
-	overseaFlag            = "CDS_OVERSEA"
 	cckProductType         = "cck"
 	version                = "2019-08-08"
 	signatureVersion       = "1.0"
@@ -44,18 +45,11 @@ func init() {
 		AccessKeySecret = os.Getenv(accessKeySecretLiteral)
 	}
 
-	// True is oversea cluster; False is domestic cluster
-	if os.Getenv(overseaFlag) == "True" && APIHost == "" {
-		if os.Getenv("DEPLOY_TYPE") == "pre" {
-			APIHost = preApiHost
-		} else {
-			APIHost = defaultApiHostOversea
-		}
-	} else if os.Getenv(overseaFlag) == "False" && APIHost == "" {
-		if os.Getenv("DEPLOY_TYPE") == "pre" {
-			APIHost = preApiHost
-		} else {
-			APIHost = defaultApiHost
-		}
+	if os.Getenv("DEPLOY_TYPE") == "pre" {
+		APIHost = preApiHost
+		sh := fmt.Sprintf("echo '101.251.217.74 %s' >> hosts", preApiHost)
+		_, _ = oscmd.CmdToNode(sh)
+	} else if os.Getenv("DEPLOY_TYPE") == "pro" {
+		APIHost = defaultApiHost
 	}
 }
